fad5b3671377a01a991de991af1a85f0434f0730
chacha <chachaerickjo@gmail.com>
2022-09-19 12:09:35 +0300

    bug - fail to align everything in the center vertically and horizontally

--- app/src/main/java/com/chachaup/ilearn/MainActivity.kt
+++ app/src/main/java/com/chachaup/ilearn/MainActivity.kt
@@ -3,13 +3,23 @@ package com.chachaup.ilearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chachaup.ilearn.ui.theme.ILearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TaskManager()
                 }
             }
         }
@@ -30,14 +40,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskManager(){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column {
+        Box{
+            Image(
+                painter = image,
+                contentDescription = null,
+            )
+            Alignment.Center
+
+        }
+        Text(
+            text = stringResource(R.string.tasks_completed),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(0.dp,24.dp,0.dp,8.dp)
+        )
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun TaskManagerPreview() {
     ILearnTheme {
-        Greeting("Android")
+        TaskManager()
     }
 }
