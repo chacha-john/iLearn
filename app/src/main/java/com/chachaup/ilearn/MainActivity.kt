08d8742aaa6d92632ba5a88e2cba80b9ab831f66
chacha <chachaerickjo@gmail.com>
2022-09-19 11:45:10 +0300

    ft - recreate the learn together app from the compose basics section of the documentation

--- app/src/main/java/com/chachaup/ilearn/MainActivity.kt
+++ app/src/main/java/com/chachaup/ilearn/MainActivity.kt
@@ -3,13 +3,19 @@ package com.chachaup.ilearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chachaup.ilearn.ui.theme.ILearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeTutorial()
                 }
             }
         }
@@ -30,14 +36,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeTutorial(){
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Box{
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+        }
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.jetpack_compose_description),
+            modifier = Modifier
+                .padding(16.dp,0.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(id = R.string.expanded_jetpack_compose_description),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ComposeTutorialPreview(){
     ILearnTheme {
-        Greeting("Android")
+        ComposeTutorial()
     }
 }
